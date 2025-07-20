7b5900bd93571326fcb8074bc6956f18abdf8d63
yosef esam <150367164+yosef-esam@users.noreply.github.com>
2025-07-20 05:04:38 +0200

    ordeers

--- src/app/[locale]/admin/orders/page.tsx
+++ src/app/[locale]/admin/orders/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 async function OrdersPage() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/order`, {
+  const res = await fetch(`/api/order`, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
